889c0a0218d65ba4b6f8b112b25576266c9f3889
vs.ivashchenkov <mrcrabs67forms@outlook.com>
2020-08-04 15:24:27 +0300

    new project

---
+++ LearnMethod/LearnMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LearnMethod
+{
+   
+    class TV
+    {
+        public bool switchedTV; //Показывает статус ТВ
+
+        public TV() { switchedTV = false; } // Конструктор
+
+        public void switchedOn() //Turn On
+        {
+            switchedTV = true;
+        }
+
+        public void switchedOff() //Turn Off
+        {
+            switchedTV = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tvstat;//status данного экземпляра класса
+            string tvinfo = "1";//string for status
+            TV myTV = new TV(); //Создание нового экземпляра
+            tvstat = myTV.switchedTV;
+
+            Console.WriteLine("Текущее состояние телевизора");
+            GetTVInfo(tvstat, ref tvinfo);
+            Console.WriteLine(tvinfo);
+
+            Console.WriteLine("Нажмите Y, если хотите включить телевизор");
+            if (Console.ReadKey(true).Key == ConsoleKey.Y)
+                myTV.switchedOn();
+
+            Console.WriteLine("Новое состояние телевизора");
+            tvstat = myTV.switchedTV;
+            GetTVInfo(tvstat, ref tvinfo);
+            Console.WriteLine(tvinfo);
+
+        }
+
+        public static void GetTVInfo(bool tvstat, ref string tvinfo)
+        {
+
+            if (tvstat == true)
+                tvinfo = "On";
+            else tvinfo = "Off";
+
+        }
+    }
+}
